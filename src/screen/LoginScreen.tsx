26c93517ecfbbb31755820eb9a917f02c626cae1
akbees <aksh08@live.com>
2023-11-15 21:47:37 +0400

    styling login user screen
    style login form and app icon on login screen
    to be implemented alert on failed login

--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,30 +1,112 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {
+  Image,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import { AuthContext } from '../service/AuthProvider';
-import { rootNavParamsType } from '../component/Navigation/RootAppNav';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
 
 function LoginScreen({ navigation, route }: any): JSX.Element {
-  const rootNavigation = useNavigation<NativeStackNavigationProp<rootNavParamsType>>();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const { login } = useContext(AuthContext);
+
+  function loginHandler() {
+    let isLogin = login(username, password);
+
+    if(!isLogin) {
+      console.log("implement alert!");
+    }
+  }
 
   return (
     <View>
-      <Text>Login</Text>
+      <View style={styles.logoIcon}>
+        <Image
+          style={styles.appLogo}
+          source={require('../images/emblem.png')}
+        />
+      </View>
+
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>Username</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+        />
+
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+      </View>
+
+      <View style={styles.formAction}>
+        <TouchableOpacity onPress={() => loginHandler()}>
+          <Text style={styles.formBtn}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  logoIcon: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 8
+  },
+  appLogo: {
+    justifyContent:'center',
+    width: 200,
+    height:200,
+  },
+  formContainer: {
+    marginVertical: 15,
+    marginBottom: 5,
+    marginHorizontal: 25,
+    paddingHorizontal: 5,
+    paddingBottom: 15,
+    flexDirection: 'column',
+    backgroundColor: "rgb(229, 228, 226)",
+    borderRadius: 8,
+  },
+  input: {
+    height: 35,
+    marginHorizontal: 12,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+  },
+  label: {
+    color: 'teal',
+    fontWeight: 'bold',
+    marginLeft: 16,
+    marginTop: 10,
+    marginBottom: 3,
+  },
+  formAction: {
+    marginTop: 8,
+    marginBottom: 5,
+    marginHorizontal: 25,
+    padding: 5,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  formBtn: {
+    color: 'white',
+    backgroundColor: 'teal',
+    width: 'auto',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
   },
 });
 
